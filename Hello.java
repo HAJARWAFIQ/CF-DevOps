edc1278541bc47441a670df025d92637a8f833e8
Hajar wafiq <wafiqhajaar@gmail.com>
2024-05-23 15:03:06 +0100

    modified the file hello

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // Affiche les messages à la console
-        System.out.println("Bonjour Master");
+        System.out.println("Bonjour Master devops");
         System.out.println("Bon courage dans votre examen");
     }
 }
