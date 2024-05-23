364c381e35b68e66462c3b5aa674aaf4fffb29eb
Hajar wafiq <wafiqhajaar@gmail.com>
2024-05-23 14:21:16 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public clas Hello {
+
+    public static void main(String[] args) {
+        // Affiche les messages à la console
+        System.out.println("Bonjour");
+        System.out.println("Bon courage dans votre examen");
+    }
+}
